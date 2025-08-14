e9273b31bd8f665a58e01dae1211b13d8e556e29
Sanya Doda <sanyaadoda@gmail.com>
2025-08-14 10:10:01 +0530

    singleton design pattern without thread, locking

---
+++ creational_design_patterns/singleton.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Singleton{
+private:
+ static Singleton* instance;
+ Singleton(){
+    cout<<"Singleton class constructor called. New obj created"<<endl;
+ }
+ public:
+ static Singleton* getInstance(){
+    if(instance==nullptr){
+        instance=new Singleton();
+    }
+    return instance;
+ }
+};
+Singleton* Singleton::instance=nullptr;
+int main(){
+    Singleton* s1=Singleton::getInstance();
+    Singleton* s2=Singleton::getInstance();
+    cout<<(s1==s2)<<endl;
+    return 0;
+}
